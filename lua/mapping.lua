31bb69a010545545ac1b4014a4569d56931e88c4
Alexander Leanovich <a.leanovich@yandex.ru>
2022-11-05 14:50:49 +0200

    remap vimspector keys

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -1,9 +1,9 @@
 -- Vimspector
 vim.cmd([[
-    nmap <F9> <cmd>call vimspector#Launch()<cr>
-    nmap <F5> <cmd>call vimspector#StepOver()<cr>
-    nmap <F8> <cmd>call vimspector#Reset()<cr>
-    nmap <F11> <cmd>call vimspector#StepOver()<cr>")
+    nmap <S-F9> <cmd>call vimspector#Launch()<cr>
+    nmap <S-F2> <cmd>call vimspector#Reset()<cr>
+    nmap <F8> <cmd>call vimspector#StepOver()<cr>
+    nmap <F9> <cmd>call vimspector#Continue()<cr>
     nmap <F12> <cmd>call vimspector#StepOut()<cr>")
     nmap <F10> <cmd>call vimspector#StepInto()<cr>")
 ]])
