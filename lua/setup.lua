b2a7ca9049224bf54f356654b636dba5b13e513e
Alexander Leanovich <a.leanovich@yandex.ru>
2022-12-13 00:21:24 +0200

    fix rust tools

--- lua/setup.lua
+++ lua/setup.lua
@@ -1,17 +1,6 @@
 require("mason").setup()
 
-local rt = {
-	server = {
-		settings = {
-			on_attach = function(_, bufnr)
-				-- Hover actions
-				vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-				-- Code action groups
-				vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-			end,
-		},
-	},
-}
+local rt = {}
 
 require("rust-tools").setup(rt)
 
