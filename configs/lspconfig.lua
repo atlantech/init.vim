35e140e5563f7d95874ca2a787d84b33e82df79a
Alexander Leanovich <a.leanovich@yandex.ru>
2023-06-03 13:29:58 +0300

    add lsp config

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,13 +1,45 @@
-local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
--- Replace <YOUR_LSP_SERVER> with each lsp server you"ve enabled.
-require("lspconfig").erlangls.setup({})
-require("lspconfig").lua_ls.setup({
-	settings = { Lua = { diagnostics = { globals = { "vim" } } } },
-})
-require("lspconfig").tsserver.setup({})
-require("lspconfig")["pyright"].setup({
-	capabilities = capabilities,
-})
-require("lspconfig").phpactor.setup({})
+local capabilities = require("plugins.configs.lspconfig").capabilities
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local lspconfig = require "lspconfig"
 
+-- todo setup each server individually
+local servers = {
+  "erlangls",
+  "tsserver",
+  "pyright",
+  "phpactor",
+  "zls",
+  "ansiblels",
+  "bashls",
+  "clangd",
+  "cmake",
+  "csharp_ls",
+  "cssls",
+  "docker_compose_language_service",
+  "dockerls",
+  "elixirls",
+  "erlangls",
+  "gopls",
+  "gradle_ls",
+  "jsonls",
+  "kotlin_language_server",
+  "phpactor",
+  "ruby_lsp",
+  "tailwindcss",
+  "tsserver",
+  "vimls",
+  "yamlls",
+}
 
+lspconfig.rust_analyzer.setup {
+  settings = {
+    ["rust-analyzer"] = {},
+  },
+}
+
+for _, server in ipairs(servers) do
+  lspconfig[server] = {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
--- plugins.lua
+++ plugins.lua
@@ -73,6 +73,10 @@ local plugins = {
   "tpope/vim-obsession",
   {
     "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-vsnip",
+      "hrsh7th/vim-vsnip",
+    },
     config = function()
       local cmp = require "cmp"
       local lspkind = require "lspkind"
@@ -143,15 +147,14 @@ local plugins = {
   "hrsh7th/cmp-nvim-lsp",
   "hrsh7th/cmp-nvim-lua",
   "hrsh7th/cmp-nvim-lsp-signature-help",
-  "hrsh7th/cmp-vsnip",
   "hrsh7th/cmp-path",
   "hrsh7th/cmp-buffer",
-  "hrsh7th/vim-vsnip",
   "puremourning/vimspector",
   { "sbdchd/neoformat", lazy = false },
   "folke/lsp-colors.nvim",
   { "catppuccin/nvim", name = "catppuccin" },
   "beanworks/vim-phpfmt",
+  "ziglang/zig.vim",
 }
 
 return plugins
