0b4948ae7f4062f69faeeae2355f9e2f6ac596d2
Alexander Leanovich <a.leanovich@yandex.ru>
2023-06-11 17:52:26 +0300

    initialize dapui

--- plugins.lua
+++ plugins.lua
@@ -292,7 +292,31 @@ local plugins = {
       "mfussenegger/nvim-dap",
     },
     init = function()
-      require("dapui").setup()
+      local dap = require "dap"
+      local dapui = require "dapui"
+
+      dapui.setup()
+
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap" },
+    config = function()
+      local opts = {
+        library = { plugins = { "nvim-dap-ui" }, type = true },
+      }
+      return opts
     end,
   },
 }
