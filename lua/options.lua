01732cf2bca630c9364498e1729bb73deee97bde
Alexander Leanovich <a.leanovich@yandex.ru>
2024-11-24 12:45:29 +0100

    add optional param

--- lua/options.lua
+++ lua/options.lua
@@ -30,4 +30,4 @@ vim.wo.foldmethod = "expr"
 vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
 vim.wo.relativenumber = true
 
-vim.api.nvim_set_option_value("clipboard", "unnamed")
+vim.api.nvim_set_option_value("clipboard", "unnamed", {})
